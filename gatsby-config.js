41c665569179b9420ae5003adfdb7dc1cf2debb6
Dustin Firebaugh <dafirebaugh@gmail.com>
2020-10-13 20:48:42 -0400

    attempting to fix gh-pages routing issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       github: `dfirebaugh`,
     },
   },
-  pathPrefix: '/dustinfirebaugh.com',
+  pathPrefix: '',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
